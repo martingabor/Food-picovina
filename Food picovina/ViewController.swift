48ab1e3e85c9849362a0948a8b8d1110db4dc9b3
MocinGejbr <gabor.martin1212@gmail.com>
2017-12-01 00:03:42 +0100

    First commit, picking photo, button - useless, textField

---
+++ Food picovina/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Food picovina
+//
+//  Created by Martin Gábor on 30/11/2017.
+//  Copyright © 2017 Martin Gábor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    //MARK: Properties
+    @IBOutlet weak var foodNameLabel: UILabel!
+    @IBOutlet weak var foodNameTextField: UITextField!
+    @IBOutlet weak var foodImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        foodNameTextField.delegate = self
+        foodImageView.isUserInteractionEnabled = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        foodNameLabel.text = textField.text
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        foodNameTextField.text = textField.text
+    }
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Image could not be picked : \(info)")
+        }
+        
+        foodImageView.image = pickedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultFoodLabel(_ sender: UIButton) {
+        foodNameLabel.text = foodNameTextField.text
+    }
+    
+    @IBAction func pickPhotoFromLibrary(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        
+        //Source where to get photos - photo library of the device
+        imagePickerController.sourceType = .photoLibrary
+        //notify 
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
